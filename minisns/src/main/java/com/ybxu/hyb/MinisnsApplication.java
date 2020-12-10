3d46486b38526d12813b90b407c1542ca02d4e33
hyb <3060540399@qq.com>
2020-12-10 16:14:50 +0800

    创建微社区项目

---
+++ minisns/src/main/java/com/ybxu/hyb/MinisnsApplication.java
@@ -0,0 +1,13 @@
+package com.ybxu.hyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinisnsApplication.class, args);
+    }
+
+}
